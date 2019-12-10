3d2243560b91a11588e9f373ec3449ce24284466
Michael Rubanov <persorub@personalics.com>
2019-12-11 01:18:56 +0200

    upgrade wip

--- generators/app/templates/test/basic.test.js
+++ generators/app/templates/test/basic.test.js
@@ -1,7 +1,9 @@
 import * as assert from 'assert';
+import { expect } from 'chai';
 
 suite('Basic tests', () => {
   test('Green test', () => {
+    expect(true).to.be.eq(true);
     assert.equal('1', true);
     assert.notDeepStrictEqual(1, true);
     assert.deepStrictEqual(true, true);
