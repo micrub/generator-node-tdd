cba892b29050b2b68d6fccec51dfa83c1a7d6006
Michael Rubanov <persorub@personalics.com>
2019-12-11 00:55:51 +0200

    eslint

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -2,8 +2,9 @@ import * as os from 'os';
 import logger from './lib/logger';
 
 const hoursUptime = () => (os.uptime() / 60 / 60).toFixed(2);
-const levelUptimeMessage = (level) => `Level log is ${level.toUpperCase()} and the system has been ${hoursUptime()} hours uptime.`;
-const createLog = (level) => ({level, message: levelUptimeMessage(level) });
+const levelUptimeMessage = level =>
+  `Level log is ${level.toUpperCase()} and the system has been ${hoursUptime()} hours uptime.`;
+const createLog = level => ({ level, message: levelUptimeMessage(level) });
 
 const logs = [
   createLog('error'),
